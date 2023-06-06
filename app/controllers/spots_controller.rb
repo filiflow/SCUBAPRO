af6ebc67a804a22ae544c25670cbe3b7cf3c60ce
Florian Berthouly <berthouly.florian@gmail.com>
2023-06-06 16:36:30 +0200

    page home + index

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -2,7 +2,6 @@ class SpotsController < ApplicationController
   def index
     @spots = Spot.all
     @markers = @spots.geocoded.map do |spot|
-      # raise
       {
         lat: spot.latitude,
         lng: spot.longitude
