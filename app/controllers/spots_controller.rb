9f59474e76b58a9f93bc23e0bba721ad8334d821
Florian Berthouly <berthouly.florian@gmail.com>
2023-06-06 15:31:22 +0200

    map en place

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -1,4 +1,12 @@
 class SpotsController < ApplicationController
   def index
+    @spots = Spot.all
+    @markers = @spots.geocoded.map do |spot|
+      # raise
+      {
+        lat: spot.latitude,
+        lng: spot.longitude
+      }
+    end
   end
 end
