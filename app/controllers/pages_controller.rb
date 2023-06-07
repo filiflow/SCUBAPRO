748b9101acaf7709b91c28bcf1ab74184aeb17b5
Florian Berthouly <berthouly.florian@gmail.com>
2023-06-07 11:07:03 +0200

    css homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-    @diving = Diving.last
+    @divings = Diving.last(3)
   end
 end
