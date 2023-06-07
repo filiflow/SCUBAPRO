7b8befe13bd294e30feb419608787f75f8eb0723
Florian Berthouly <berthouly.florian@gmail.com>
2023-06-07 15:35:31 +0200

    ok

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -13,7 +13,7 @@ export default class extends Controller {
 
     this.map = new mapboxgl.Map({
       container: this.element,
-      style: "mapbox://styles/mapbox/satellite-streets-v12"
+      style: "mapbox://styles/flo-goon/clilpunq200ix01pg1nxr5gr3"
     })
 
     this.#addMarkersToMap()
