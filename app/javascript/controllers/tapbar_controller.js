a7256cdd32585288dcf32153b7e729aa77774732
Marianne Brandely <119945585+Chaffinchmb@users.noreply.github.com>
2023-06-08 16:09:42 +0200

    homepage navbar

--- app/javascript/controllers/tapbar_controller.js
+++ app/javascript/controllers/tapbar_controller.js
@@ -3,12 +3,16 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="tapbar"
 export default class extends Controller {
   connect() {
+    console.log("coucou")
   }
 
-  const preactiveItem = document.querySelector('.pre-active')
-  const barItems = document.querySelectorAll('.bar-item')
 
-  function changeActive (newActive) {
+  changeActive(event) {
+    const newActive = event.currentTarget
+    console.log(newActive);
+    const preactiveItem = document.querySelector('.pre-active')
+    const barItems = document.querySelectorAll('.bar-item')
+
     preactiveItem.classList.remove('pre-active')
     barItems.forEach(element => {
       element.classList.remove('active')
