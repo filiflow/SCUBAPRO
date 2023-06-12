ab2f5f975c13f30cb15041dfd58eb77b09708e77
Marianne Brandely <119945585+Chaffinchmb@users.noreply.github.com>
2023-06-12 19:36:09 +0200

    seed animals

--- app/models/participation.rb
+++ app/models/participation.rb
@@ -2,7 +2,7 @@ class Participation < ApplicationRecord
   belongs_to :user
   belongs_to :diving
   has_many_attached :photos
-  has_many :presences, :as => presentable
+  # has_many :presences, :as => presentable
 
   validates :depth, presence: true
   validates :gas, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_12_103657) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_12_141529) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -47,6 +47,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_12_103657) do
     t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "description"
   end
 
   create_table "divings", force: :cascade do |t|
