35bffa33350a05275a886c9e761b2971be03b0fd
Florian Berthouly <berthouly.florian@gmail.com>
2023-06-14 18:19:02 +0200

    à finir

--- app/javascript/controllers/weather_controller.js
+++ app/javascript/controllers/weather_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="weather"
 export default class extends Controller {
-  static targets = ["description", "temperature", "city", "icon"]
+  static targets = ["description", "temperature", "city", "icon", "link"]
   connect() {
     this.apiKey = "ae4bc35f1ebe8d4a3bfe20e7e204b496"
     // const userGeoloc = this.findGeoloc()
@@ -17,12 +17,12 @@ export default class extends Controller {
   }
 
   getGeolocAndfetchWeather(){
-      navigator.geolocation.getCurrentPosition((position) => {
-          var latitude = position.coords.latitude;
-          var longitude = position.coords.longitude;
-          var altitude = position.coords.altitude;
-          this.fetchWeather(latitude, longitude)
-        });
+    navigator.geolocation.getCurrentPosition((position) => {
+        var latitude = position.coords.latitude;
+        var longitude = position.coords.longitude;
+        var altitude = position.coords.altitude;
+        this.fetchWeather(latitude, longitude)
+      });
   }
 
   #insertWeatherData(data){
@@ -30,5 +30,6 @@ export default class extends Controller {
     this.descriptionTarget.innerText = data.weather[0].main
     this.temperatureTarget.innerText = `${Math.round(data.main.temp)} °C`
     this.cityTarget.innerText = data.name
+    this.linkTarget.classList.add("d-none")
   }
 }
